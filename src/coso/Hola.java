4db66bd299ef46486a582f6d9b9d65a6ec80275e
Marina Torre Duran <Marina Torre Duran@LAPTOP-7AN9ONCM>
2025-04-29 20:04:19 +0200

    Aplicación subida

---
+++ src/coso/Hola.java
@@ -0,0 +1,14 @@
+package coso;
+
+public class Hola {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 10;
+		int resultado = a + b;
+		
+		System.out.println("La suma de " + a + " + " + b + " = " + resultado);
+
+	}//fin main
+
+}//fin clase
